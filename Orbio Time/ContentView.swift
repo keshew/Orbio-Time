28fa75672524e03411f6cf37ceba477f62464807
keshew <artyom.korotckov2017@yandex.ru>
2025-12-09 21:23:46 +0500

    Initial Commit

---
+++ Orbio Time/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Orbio Time
+//
+//  Created by Артём Коротков on 09.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
